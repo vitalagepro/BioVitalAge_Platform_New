33d73af6b420d9cb358f9b04c5c6fec1541a9ce2
Luigi Balestrucci <luigi@spicysparks.com>
2024-12-16 13:44:04 +0100

    Add front-end features

--- BioVitalAge/static/includes/js/HomePage.js
+++ BioVitalAge/static/includes/js/HomePage.js
@@ -3,13 +3,13 @@ const userModal = document.getElementById("userModal");
 const userModalBtn = document.getElementById("nav-bar-user-modal-btn");
 
 function showModal() {
-  userModal.style.display = "block";
+  userModal.classList.add("show");
 }
 
 userImg.addEventListener("mouseover", showModal);
 
 userModal.addEventListener("mouseout", () => {
-  userModal.style.display = "none";
+  userModal.classList.remove("show");
 });
 
 userModalBtn.addEventListener("mouseover", showModal);
@@ -60,14 +60,27 @@ const patientsCtx = document.getElementById("patientsChart");
 new Chart(patientsCtx, {
   type: "bar",
   data: {
-    labels: ["Gen", "Feb", "Mar", "Apr", "Mag", "Giu"],
+    labels: [
+      "Gen",
+      "Feb",
+      "Mar",
+      "Apr",
+      "Mag",
+      "Giu",
+      "Lug",
+      "Ago",
+      "Set",
+      "Ott",
+      "Nov",
+      "Dic",
+    ],
     datasets: [
       {
         label: "Pazienti Inseriti",
         data: [30, 45, 60, 50, 70, 90],
         backgroundColor: "#3a255d",
-        categoryPercentage: 0.8, // Riduce lo spazio tra le categorie
-        barPercentage: 0.9, // Riduce lo spazio interno tra le barre
+        categoryPercentage: 2.8, // Maggior spazio tra le categorie
+        barPercentage: 0.1, // Barre più sottili
       },
     ],
   },
@@ -155,4 +168,284 @@ let monthlyPatientsChart = new Chart(monthlyCtx, {
       }
     },
   },
+});
+
+/*  -----------------------------------------------------------------------------------------------
+  JS SIDEBAR
+--------------------------------------------------------------------------------------------------- */
+document.addEventListener("DOMContentLoaded", () => {
+  const sidebar = document.querySelectorAll("#sidebar");
+  const sidebarTitle = document.getElementById("sidebar-title");
+  const sidebarContent = document.getElementById("sidebar-content");
+  const closeSidebar = document.getElementById("closeSidebar");
+  const bgSidebar = document.querySelector(".bg-sidebar");
+
+  // Gestisci il click sulle icone
+  document.querySelectorAll(".sidebar-trigger").forEach((trigger) => {
+    trigger.addEventListener("click", (event) => {
+      event.preventDefault();
+      const section = trigger.getAttribute("data-section");
+
+      // Aggiorna contenuto della sidebar in base alla sezione
+      sidebarTitle.textContent = section;
+      switch (section) {
+        case "Notifiche":
+          sidebarContent.innerHTML = `
+            <div id="notification-list">
+              <h3 class="title-notice">Ultime Novità</h3>
+              <a href="#" class="blog-link" title="View Blog">
+                <div class="blog-container">
+                  <div class="container_img">
+                    <img
+                      src="../../static/includes/images/search-patients-item.webp"
+                      alt="img-blog"
+                    />
+                  </div>
+                  <div>
+                    <h3 class="blog-title">Blog 1</h3>
+                    <p class="blog-text">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Adipisci at veniam eum quo alias suscipit corrupti,
+                      commodi eveniet porro nulla magni a ipsum voluptate, totam
+                      rerum? Ducimus ipsum quibusdam quam.
+                    </p>
+                  </div>
+                </div>
+              </a>
+              <a href="#" class="blog-link" title="View Blog">
+                <div class="blog-container">
+                  <div class="container_img">
+                    <img
+                      src="../../static/includes/images/search-patients-item.webp"
+                      alt="img-blog"
+                    />
+                  </div>
+                  <div>
+                    <h3 class="blog-title">Blog 2</h3>
+                    <p class="blog-text">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Adipisci at veniam eum quo alias suscipit corrupti,
+                      commodi eveniet porro nulla magni a ipsum voluptate, totam
+                      rerum? Ducimus ipsum quibusdam quam.
+                    </p>
+                  </div>
+                </div>
+              </a>
+              <a href="#" class="blog-link" title="View Blog">
+                <div class="blog-container">
+                  <div class="container_img">
+                    <img
+                      src="../../static/includes/images/search-patients-item.webp"
+                      alt="img-blog"
+                    />
+                  </div>
+                  <div>
+                    <h3 class="blog-title">Blog 3</h3>
+                    <p class="blog-text">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Adipisci at veniam eum quo alias suscipit corrupti,
+                      commodi eveniet porro nulla magni a ipsum voluptate, totam
+                      rerum? Ducimus ipsum quibusdam quam.
+                    </p>
+                  </div>
+                </div>
+              </a>
+              <h3 class="title-notice">Altre Novità</h3>
+              <a href="#" class="blog-link" title="View Blog">
+                <div class="blog-container">
+                  <div class="container_img">
+                    <img
+                      src="../../static/includes/images/search-patients-item.webp"
+                      alt="img-blog"
+                    />
+                  </div>
+                  <div>
+                    <h3 class="blog-title">Blog 1</h3>
+                    <p class="blog-text">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Adipisci at veniam eum quo alias suscipit corrupti,
+                      commodi eveniet porro nulla magni a ipsum voluptate, totam
+                      rerum? Ducimus ipsum quibusdam quam.
+                    </p>
+                  </div>
+                </div>
+              </a>
+              <a href="#" class="blog-link" title="View Blog">
+                <div class="blog-container">
+                  <div class="container_img">
+                    <img
+                      src="../../static/includes/images/search-patients-item.webp"
+                      alt="img-blog"
+                    />
+                  </div>
+                  <div>
+                    <h3 class="blog-title">Blog 2</h3>
+                    <p class="blog-text">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Adipisci at veniam eum quo alias suscipit corrupti,
+                      commodi eveniet porro nulla magni a ipsum voluptate, totam
+                      rerum? Ducimus ipsum quibusdam quam.
+                    </p>
+                  </div>
+                </div>
+              </a>
+              <a href="#" class="blog-link" title="View Blog">
+                <div class="blog-container">
+                  <div class="container_img">
+                    <img
+                      src="../../static/includes/images/search-patients-item.webp"
+                      alt="img-blog"
+                    />
+                  </div>
+                  <div>
+                    <h3 class="blog-title">Blog 3</h3>
+                    <p class="blog-text">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Adipisci at veniam eum quo alias suscipit corrupti,
+                      commodi eveniet porro nulla magni a ipsum voluptate, totam
+                      rerum? Ducimus ipsum quibusdam quam.
+                    </p>
+                  </div>
+                </div>
+              </a>
+              <a href="#" class="blog-link" title="View Blog">
+                <div class="blog-container">
+                  <div class="container_img">
+                    <img
+                      src="../../static/includes/images/search-patients-item.webp"
+                      alt="img-blog"
+                    />
+                  </div>
+                  <div>
+                    <h3 class="blog-title">Blog 1</h3>
+                    <p class="blog-text">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Adipisci at veniam eum quo alias suscipit corrupti,
+                      commodi eveniet porro nulla magni a ipsum voluptate, totam
+                      rerum? Ducimus ipsum quibusdam quam.
+                    </p>
+                  </div>
+                </div>
+              </a>
+              <a href="#" class="blog-link" title="View Blog">
+                <div class="blog-container">
+                  <div class="container_img">
+                    <img
+                      src="../../static/includes/images/search-patients-item.webp"
+                      alt="img-blog"
+                    />
+                  </div>
+                  <div>
+                    <h3 class="blog-title">Blog 2</h3>
+                    <p class="blog-text">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Adipisci at veniam eum quo alias suscipit corrupti,
+                      commodi eveniet porro nulla magni a ipsum voluptate, totam
+                      rerum? Ducimus ipsum quibusdam quam.
+                    </p>
+                  </div>
+                </div>
+              </a>
+              <a href="#" class="blog-link" title="View Blog">
+                <div class="blog-container">
+                  <div class="container_img">
+                    <img
+                      src="../../static/includes/images/search-patients-item.webp"
+                      alt="img-blog"
+                    />
+                  </div>
+                  <div>
+                    <h3 class="blog-title">Blog 3</h3>
+                    <p class="blog-text">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Adipisci at veniam eum quo alias suscipit corrupti,
+                      commodi eveniet porro nulla magni a ipsum voluptate, totam
+                      rerum? Ducimus ipsum quibusdam quam.
+                    </p>
+                  </div>
+                </div>
+              </a>
+              <a href="#" class="blog-link" title="View Blog">
+                <div class="blog-container">
+                  <div class="container_img">
+                    <img
+                      src="../../static/includes/images/search-patients-item.webp"
+                      alt="img-blog"
+                    />
+                  </div>
+                  <div>
+                    <h3 class="blog-title">Blog 1</h3>
+                    <p class="blog-text">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Adipisci at veniam eum quo alias suscipit corrupti,
+                      commodi eveniet porro nulla magni a ipsum voluptate, totam
+                      rerum? Ducimus ipsum quibusdam quam.
+                    </p>
+                  </div>
+                </div>
+              </a>
+              <a href="#" class="blog-link" title="View Blog">
+                <div class="blog-container">
+                  <div class="container_img">
+                    <img
+                      src="../../static/includes/images/search-patients-item.webp"
+                      alt="img-blog"
+                    />
+                  </div>
+                  <div>
+                    <h3 class="blog-title">Blog 2</h3>
+                    <p class="blog-text">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Adipisci at veniam eum quo alias suscipit corrupti,
+                      commodi eveniet porro nulla magni a ipsum voluptate, totam
+                      rerum? Ducimus ipsum quibusdam quam.
+                    </p>
+                  </div>
+                </div>
+              </a>
+              <a href="#" class="blog-link" title="View Blog">
+                <div class="blog-container">
+                  <div class="container_img">
+                    <img
+                      src="../../static/includes/images/search-patients-item.webp"
+                      alt="img-blog"
+                    />
+                  </div>
+                  <div>
+                    <h3 class="blog-title">Blog 3</h3>
+                    <p class="blog-text">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Adipisci at veniam eum quo alias suscipit corrupti,
+                      commodi eveniet porro nulla magni a ipsum voluptate, totam
+                      rerum? Ducimus ipsum quibusdam quam.
+                    </p>
+                  </div>
+                </div>
+              </a>
+            </div>
+            `;
+          break;
+        case "Email":
+          sidebarContent.innerHTML = "<p>Qui trovi tutte le email.</p>";
+          break;
+        case "Update":
+          sidebarContent.innerHTML = "<p>Qui trovi gli aggiornamenti.</p>";
+          break;
+        default:
+          sidebarContent.innerHTML = "<p>Contenuto non disponibile.</p>";
+      }
+
+      // Mostra la sidebar
+      document.body.classList.add("visible");
+    });
+  });
+
+  // Chiudi la sidebar
+  bgSidebar.addEventListener("click", () => {
+    document.body.classList.remove("visible");
+  });
+
+  closeSidebar.addEventListener("click", () => {
+    document.body.classList.remove("visible");
+  });
 });
--- BioVitalAge/static/includes/js/calcolatore.js
+++ BioVitalAge/static/includes/js/calcolatore.js
@@ -1,6 +1,22 @@
-document.querySelectorAll('input[type="range"]').forEach(slider => {
-    slider.addEventListener('input', function() {
-        const value = (this.value - this.min) / (this.max - this.min) * 100;
-        this.style.setProperty('--value', `${value}%`);
-    });
+document.querySelectorAll('input[type="range"]').forEach((slider) => {
+  slider.addEventListener("input", function () {
+    const value = ((this.value - this.min) / (this.max - this.min)) * 100;
+    this.style.setProperty("--value", `${value}%`);
+  });
 });
+
+const userImg = document.getElementById("userImg");
+const userModal = document.getElementById("userModal");
+const userModalBtn = document.getElementById("nav-bar-user-modal-btn");
+
+function showModal() {
+  userModal.classList.add("show");
+}
+
+userImg.addEventListener("mouseover", showModal);
+
+userModal.addEventListener("mouseout", () => {
+  userModal.classList.remove("show");
+});
+
+userModalBtn.addEventListener("mouseover", showModal);
