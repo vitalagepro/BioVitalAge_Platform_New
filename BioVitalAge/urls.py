b0f50d79bb62402b2f03857f5278cfc23cf614be
Luigi Balestrucci <luigi@spicysparks.com>
2024-12-20 15:00:09 +0100

    Add dynamic anagraphic data in 'Dati Base'

--- BioVitalAge/urls.py
+++ BioVitalAge/urls.py
@@ -8,7 +8,8 @@ urlpatterns = [
     path("Calcolatore_Page", views.CalcolatoreRender.as_view(), name='Calcolatore'),
     path("Risultati_Page", views.RisultatiRender.as_view(), name='Risultati'),
     path("persona/<int:id>/", views.PersonaDetailView.as_view(), name="persona_detail"),
-    path("CartellaPaziente/<int:id>/", views.CartellaPazienteView.as_view(), name="cartella_paziente")
+    path("CartellaPaziente/<int:id>/", views.CartellaPazienteView.as_view(), name="cartella_paziente"),
+    path("DatiBase/<int:id>/", views.DatiBaseView.as_view(), name="dati_base"),
 ]
 
 
