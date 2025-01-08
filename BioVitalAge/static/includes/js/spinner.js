6291b6e94ccd2c0d9509f5ef09e025a0ce4a4cdc
vito di venosa <110898637+Vito290500@users.noreply.github.com>
2025-01-08 12:18:28 +0100

    aggiunte delle modifiche Vito

---
+++ BioVitalAge/static/includes/js/spinner.js
@@ -0,0 +1,8 @@
+// Funzione che imposta "opacity: 0" e "z-index: -1" dopo 3 secondi
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(() => {
+      document.getElementById("loading-wrapper").style.opacity = "0";
+      document.getElementById("loading-wrapper").style.zIndex = "-1";
+    }, 400);
+  });
+  
