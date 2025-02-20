d337279867d932779914fd27653dd05f0b690377
vito di venosa <110898637+Vito290500@users.noreply.github.com>
2025-02-20 15:55:14 +0100

    fix an issue con il calcolo della capacità vitale

--- BioVitalAge/utils.py
+++ BioVitalAge/utils.py
@@ -662,7 +662,7 @@ def calculate_biological_age(chronological_age, d_roms = 260, osi = 10, pat = 25
 
 #DOMINIO COGNITIVO 
 def Calcolo_Dominio_Cognitivo(Somma_MMSE):
-
+   
     if int(Somma_MMSE) >= 25:
         return 0
     elif int(Somma_MMSE) >= 18 and int(Somma_MMSE) <= 24:
@@ -681,7 +681,7 @@ def Calcolo_GDS(Somma_GDS):
         return 1
     
 def Calcolo_Locus_of_control(Somma_LOC):
-
+  
     if int(Somma_LOC) >= 30 and int(Somma_LOC) <= 40:
         return 0
     elif int(Somma_LOC) >= 19 and int(Somma_LOC) <= 29:
@@ -700,7 +700,7 @@ def Calcolo_Vista(Somma_Vista):
         return 1
 
 def Calcolo_Udito(Somma_Udito):
- 
+   
     if int(Somma_Udito) >= 6:
         return 0
     elif int(Somma_Udito) >= 3 and int(Somma_Udito) <= 5:
@@ -710,7 +710,7 @@ def Calcolo_Udito(Somma_Udito):
     
 #DOMINIO DELLA VITALIA'
 def Calcolo_HGS(HGS):
-  
+
     if str(HGS) == 'Strong':
         return 0
     elif str(HGS) == 'Normal':
@@ -719,17 +719,16 @@ def Calcolo_HGS(HGS):
         return 1
 
 def Calcolo_Peak_Flow(PFT):
-
+   
     if int(PFT) >= 90 and int(PFT) <= 100:
         return 0
     elif int(PFT) >= 50 and int(PFT) <= 80:
         return 0.5
-    elif int(PFT) < 5:
+    elif int(PFT) < 50:
         return 1
     
 def Calcolo_ISQ(ISQ): 
- 
-    print(ISQ)
+   
     if int(ISQ) >= 0 and int(ISQ) <= 8:
         return 0
     elif int(ISQ) >= 9 and int(ISQ) <= 18:
@@ -738,7 +737,7 @@ def Calcolo_ISQ(ISQ):
         return 1
     
 def Calcolo_BMI(BMI):
- 
+  
     if float(BMI) >= 18.5 and float(BMI) <= 24.9:
         return 0
     elif float(BMI) >= 16 and float(BMI) <= 18.4 or float(BMI) <= 25 and float(BMI) >= 29.9:
@@ -747,7 +746,7 @@ def Calcolo_BMI(BMI):
         return 1
     
 def Calcolo_Circoferenza_del_polpaccio_W(CDP):
-    
+   
     if int(CDP) > 33:
         return 0
     
@@ -758,7 +757,7 @@ def Calcolo_Circoferenza_del_polpaccio_W(CDP):
         return 1
 
 def Calcolo_Circoferenza_del_polpaccio_M(CDP):
-  
+    
     if int(CDP) > 35:
         return 0
     
@@ -769,7 +768,7 @@ def Calcolo_Circoferenza_del_polpaccio_M(CDP):
         return 1
 
 def Calcolo_WHR_M(WHR):
-  
+    
     if float(WHR) < 0.90:
         return 0
     elif float(WHR) >= 0.90 and float(WHR) <= 1.00:
@@ -778,7 +777,7 @@ def Calcolo_WHR_M(WHR):
         return 1 
 
 def Calcolo_WHR_W(WHR):
-   
+ 
     if float(WHR) < 0.75:
         return 0
     elif float(WHR) >= 0.75 and float(WHR) <= 0.85:
@@ -797,7 +796,7 @@ def Calcolo_Weist_Height_Ratio(WHR_Ratio):
     
 #DOMIONIO DELLA LOCOMOZIONE
 def Calcolo_CST(CST):
-   
+
     if float(CST) > 0.50:
         return 0
     elif float(CST) >= 0.35 and float(CST) <= 0.50:
@@ -826,7 +825,7 @@ def Calcolo_PPT(PPT):
         return 1
     
 def Calcolo_Sarc_F(SARC_F):
-  
+ 
     if float(SARC_F) >= 4:
         return 0
     if float(SARC_F) < 4:
